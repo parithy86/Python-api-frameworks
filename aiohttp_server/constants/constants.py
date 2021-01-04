3a1aef4eca64ba8e3b3addc77407378eb3e67093
parithy86 <parithy.s@gmail.com>
2021-01-03 20:47:27 -0700

    Added API Endpoint configs

--- aiohttp_server/constants/constants.py
+++ aiohttp_server/constants/constants.py
@@ -1,3 +1,12 @@
 class Constants:
     RESPONSE_CODES_SUCCESS = [200, 201]
     RESPONSE_CODES_FAILURE = [400, 401, 404, 500]
+    URL = "https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&tickers=false&market_data=false&community_data=false&developer_data=false&sparkline=false"
+    CONTENT_TYPE = "application/json"
+
+    SERVICE_CONFIG = {
+        "crypto_info": {
+            "function_name": "crypto_info",
+            "execute_method": "get_crypto_details"
+        }
+    }
